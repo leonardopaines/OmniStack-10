ca78f18e7ae5a0126744654c2babbac9cdc94423
Leonardo Paines <leonardopaines@gboex.com.br>
2020-01-15 16:47:53 -0300

    3° aula - Inicio da parte web(client)

---
+++ web/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+// Componente: Bloco isolado de HTML, CSS e JS, o qual não interfere no restante da aplicação
+// Propriedade: Informações que um componente pai passa para o componente filho
+// Estado: Informações mantidas pelo componentes (Lembrar: imutabilidade)
+
+function App() {
+  return (
+    <div id="app">
+      <aside>
+
+      </aside>
+      <main>
+
+      </main>
+    </div>
+  );
+}
+
+export default App;
